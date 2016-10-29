1efc1d14d06998508e4bbc39ae0135419e2fde3d
HuyTCM <huytcm.1708@gmail.com>
2016-10-29 20:22:13 +0700

    create new model for response to user

---
+++ capstone/chatbot/src/main/java/com/fu/bot/model/ResponeObj.java
@@ -0,0 +1,39 @@
+package com.fu.bot.model;
+
+import java.util.List;
+
+/**
+ * Created by HuyTCM on 10/29/16.
+ */
+public class ResponeObj {
+
+    private String name;
+
+    private String message;
+
+    private List<ProductObj> productObjList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<ProductObj> getProductObjList() {
+        return productObjList;
+    }
+
+    public void setProductObjList(List<ProductObj> productObjList) {
+        this.productObjList = productObjList;
+    }
+}
